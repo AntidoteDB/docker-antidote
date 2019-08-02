5ee817e2074f7cd2f48c03c1af27f96a637feb24
Jonathan Sid-Otmane <watabanga@gmail.com>
2019-08-02 15:05:31 +0200

    Resolves race condition during startup

--- latest/start_and_attach.sh
+++ latest/start_and_attach.sh
@@ -4,7 +4,18 @@ NPROC=$(nproc)
 
 if [[ ${NPROC} -ge 2 ]]; then
   trap "echo 'Shutting down' && /opt/antidote/bin/env stop" TERM
-  /opt/antidote/bin/env start && sleep 10 && tail -f /opt/antidote/log/console.log & wait ${!}
+  /opt/antidote/bin/env start &
+  
+  while [ ! -f /opt/antidote/log/console.log ]
+  do
+	  echo -n '.'
+	  sleep 1
+  done
+  echo '.'
+
+  tail -f /opt/antidote/log/console.log &
+  wait ${!}
+
 else
   echo "You need at least 2 cpu cores in your Docker (virtual) machine to run this image!"
   echo "You have ${NPROC} processors"
