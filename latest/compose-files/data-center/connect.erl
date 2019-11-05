806486a2c142f76fcca1a1f8e82cba4dfdcd2f5d
Albert Schimpf <git@schimpf.link>
2019-11-05 15:58:38 +0100

    local dockerfile updated, more doc

--- latest/compose-files/data-center/connect.erl
+++ latest/compose-files/data-center/connect.erl
@@ -1,6 +1,9 @@
 #!/usr/bin/env escript
 %%! -smp enable -sname erlshell -setcookie secret
 main(Args) ->
+    % hack until #396 is fixed
+    % if node is still starting, retry
+    % if another error happens (already a dc), stop
     E = (catch rpc:call(antidote@dc1n1, antidote_dc_manager, create_dc, [[antidote@dc1n1, antidote@dc1n2, antidote@dc1n3]])),
     case E of
         ok -> io:format("Data centers connected!~n");
